2e6936ea6db341277c5b9a945f04843d64a90424
dhrubo020 <44555051+dhrubo020@users.noreply.github.com>
2019-12-21 21:59:32 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /**
 * Module dependencies.
-*/
+
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
@@ -41,3 +41,31 @@ app.post('/send',routes.call_send);
 */
 app.get('/',"hello");
 app.listen(8080);
+//-------------------------------------------------------------------------------
+*/
+
+// using the http module
+let http = require('http'),
+ 
+// look for PORT environment variable, 
+// else look for CLI argument,
+// else use hard coded value for port 8080
+port = process.env.PORT || process.argv[2] || 8080;
+ 
+// create a simple server
+let server = http.createServer(function (req, res) {
+ 
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('hello heroku!', 'utf-8');
+        res.end();
+ 
+    });
+ 
+// listen on the port
+server.listen(port, function () {
+ 
+    console.log('app up on port: ' + port);
+ 
+});
